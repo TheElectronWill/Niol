efd3db4390d7b2a277a7565fd8d348ea0a7c70ef
Guillaume R <theelectronwill@gmail.com>
2018-08-28 23:54:46 +0200

    Reorganize the BufferSettings

--- main/src/com/electronwill/niol/network/tcp/BufferSettings.scala
+++ main/src/com/electronwill/niol/network/tcp/BufferSettings.scala
@@ -4,23 +4,23 @@ import com.electronwill.niol.buffer.provider.BufferProvider
 
 /**
  *
- * @param preTransformReadSize        the size of the read buffer, if there is a data transformation
- * @param packetBufferBaseSize        the size of the packet buffer which, if there is no transformation, is also the read buffer
- * @param readBufferProvider          the provider of the read buffer
- * @param postTransformBufferProvider the provider of the packet buffer, if there is a data transformation
+ * @param packetBufferBaseSize the size of the packet buffer
+ * @param packetBufferProvider the provider of the packet buffer
+ * @param readBufferSize       the size of the read buffer, used when there's a [[BufferTransform]]
+ * @param readBufferProvider   the provider of the read buffer, used when there's a [[BufferTransform]]
  */
 final case class BufferSettings(
-    preTransformReadSize: Int,
     packetBufferBaseSize: Int,
-    readBufferProvider: BufferProvider,
-    postTransformBufferProvider: BufferProvider) {
+    packetBufferProvider: BufferProvider,
+    readBufferSize: Int,
+    readBufferProvider: BufferProvider) {
 
   def this(baseBufferSize: Int, bufferProvider: BufferProvider) = {
-    this(baseBufferSize, baseBufferSize, bufferProvider, bufferProvider)
+    this(baseBufferSize, bufferProvider, baseBufferSize, bufferProvider)
   }
 }
 object BufferSettings {
-  def apply(baseBufferSize: Int, bufferProvider: BufferProvider):BufferSettings = {
-    apply(baseBufferSize, baseBufferSize, bufferProvider, bufferProvider)
+  def apply(baseBufferSize: Int, bufferProvider: BufferProvider): BufferSettings = {
+    apply(baseBufferSize, bufferProvider, baseBufferSize, bufferProvider)
   }
 }
