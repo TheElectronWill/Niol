a3831c525d01e9988104c2d54739c76f32f5e169
Guillaume R <theelectronwill@gmail.com>
2019-03-16 22:16:18 +0100

    Document Bytes class

--- main/src/com/electronwill/niol/network/tcp/package.scala
+++ main/src/com/electronwill/niol/network/tcp/package.scala
@@ -1,6 +1,11 @@
 package com.electronwill.niol.network
 
 package object tcp {
+
+  /**
+   * @param array an array containing the data, starting at index 0
+   * @param length the length of the data
+   */
   final case class Bytes(array: Array[Byte], length: Int)
 
   type BytesTransform = Bytes => Bytes
